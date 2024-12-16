4f60a4b2209acff669ce6fa4212996b634588c0a
*vivek <vrvivekofficial@gmail.com>
2024-12-16 10:44:06 +0530

    configure the middlewares and connect to the database

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 import { config } from "dotenv";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 config();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp/",
+  })
+);
+
+connectDB()
 
 export default app;
