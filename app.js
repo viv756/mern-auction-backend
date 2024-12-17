475f012d2bb03b6c3b9147bff331a1e165789bfe
*vivek <vrvivekofficial@gmail.com>
2024-12-17 11:20:51 +0530

    create register api route and create errorhandle middleware and fixed a bug in server.js file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { connectDB } from "./lib/db.js";
+import { errorMiddleware } from "./middlewares/errorHandler.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 config();
@@ -17,7 +19,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   fileUpload({
@@ -26,6 +28,10 @@ app.use(
   })
 );
 
-connectDB()
+app.use("/api/v1/user", userRouter);
+
+connectDB();
+
+app.use(errorMiddleware);
 
 export default app;
---
+++ controllers/user.controller.js
@@ -0,0 +1,91 @@
+import ErrorHandler from "../middlewares/errorHandler.js";
+import cloudinary from "../lib/cloudinary.js";
+import User from "../models/user.model.js";
+
+export const register = async (req, res, next) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("Profile Image Required", 400));
+  }
+
+  const { profileImage } = req.files;
+  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+  if (!allowedFormats.includes(profileImage.mimetype)) {
+    return next(new ErrorHandler("File format not supported.", 400));
+  }
+
+  const {
+    userName,
+    email,
+    password,
+    phone,
+    address,
+    role,
+    bankAccountNumber,
+    bankAccountName,
+    bankName,
+    easypaisaAccountNumber,
+    paypalEmail,
+  } = req.body;
+
+  console.log(req.body);
+
+  if (!userName || !email || !phone || !password || !address || !role) {
+    return next(new ErrorHandler("Please fill full form.", 400));
+  }
+
+  if (role === "Auctioneer") {
+    if (!bankAccountName || !bankAccountNumber || !bankName) {
+      return next(new ErrorHandler("Please provide your full bank details.", 400));
+    }
+    if (!easypaisaAccountNumber) {
+      return next(new ErrorHandler("Please provide your easypaisa account number.", 400));
+    }
+    if (!paypalEmail) {
+      return next(new ErrorHandler("Please provide your paypal email.", 400));
+    }
+  }
+
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(new ErrorHandler("User already registered.", 400));
+  }
+
+  const cloudinaryResponse = await cloudinary.uploader.upload(profileImage.tempFilePath, {
+    folder: "MERN_AUCTION_PLATFORM_USERS",
+  });
+
+  if (!cloudinaryResponse || cloudinaryResponse.error) {
+    console.error("Cloudinary error:", cloudinaryResponse.error || "Unknown cloudinary error.");
+    return next(new ErrorHandler("Failed to upload profile image to cloudinary.", 500));
+  }
+
+  const user = await User.create({
+    userName,
+    email,
+    password,
+    phone,
+    address,
+    role,
+    profileImage: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+    paymentMethods: {
+      bankTransfer: {
+        bankAccountNumber,
+        bankAccountName,
+        bankName,
+      },
+      easypaisa: {
+        easypaisaAccountNumber,
+      },
+      paypal: {
+        paypalEmail,
+      },
+    },
+  });
+  res.status(201).json({
+    success: true,
+    message: "User Registered",
+  });
+};
