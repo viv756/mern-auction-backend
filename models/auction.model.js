6a24ffaecb7d000120b2eed9835fefa86244379f
*vivek <vrvivekofficial@gmail.com>
2024-12-18 11:48:57 +0530

    create auction model

---
+++ models/auction.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const auctionSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  startingBid: Number,
+  currentBid: { type: Number, default: 0 },
+  startTime: String,
+  endTime: String,
+  image: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  bids: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Bid",
+      },
+      userName: String,
+      profileImage: String,
+      amount: Number,
+    },
+  ],
+  highestBidder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  commissionCalculated: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Auction = mongoose.model("Auction", auctionSchema);
+export default Auction;
