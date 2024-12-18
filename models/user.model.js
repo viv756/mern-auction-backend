9f8ec4ecc42c6173b3fcbd25ddb8f15bcb2435f6
*vivek <vrvivekofficial@gmail.com>
2024-12-18 08:38:36 +0530

    create generatejwttoken method

--- models/user.model.js
+++ models/user.model.js
@@ -76,5 +76,11 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 export default User;
