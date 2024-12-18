4bb4d99db26ce31abaa05888ba50b8903b50c769
*vivek <vrvivekofficial@gmail.com>
2024-12-18 12:47:10 +0530

    create a middleware to check the user role

--- routes/auctionItem.route.js
+++ routes/auctionItem.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { isAuthenticated } from "../middlewares/auth.js";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 import { addNewAuctionItem } from "../controllers/auctionItem.controller.js";
 
 const router = express.Router();
 
-router.post("/create", isAuthenticated, addNewAuctionItem);
+router.post("/create", isAuthenticated, isAuthorized("Auctioneer"), addNewAuctionItem);
 
 export default router;
