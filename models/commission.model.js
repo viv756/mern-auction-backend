8cf906fb8373e609b8727506d9d6e7a5a56537f1
*vivek <vrvivekofficial@gmail.com>
2024-12-21 12:15:00 +0530

    create commission model

---
+++ models/commission.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const commissionSchema = new mongoose.Schema({
+  amount: Number,
+  user: mongoose.Schema.Types.ObjectId,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Commission = mongoose.model("Commission", commissionSchema);
+export default Commission
