cecf8f6e6ffc1bbd6aae435e16491a5ef260b187
*vivek <vrvivekofficial@gmail.com>
2024-12-24 13:35:03 +0530

    cleared typos

--- middlewares/trackCommissionStatus.js
+++ middlewares/trackCommissionStatus.js
@@ -2,12 +2,12 @@ import User from "../models/user.model.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import { errorMiddleware } from "./errorHandler.js";
 
-export const trackCommissionStatus = catchAsyncErrors(async (req, resizeBy, next) => {
+export const trackCommissionStatus = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user._id);
   if (user.unpaidCommission > 0) {
     return next(
       new errorMiddleware(
-        "You have unpaid commissions. Please pay them before posting a new auctio",
+        "You have unpaid commissions. Please pay them before posting a new auction",
         403
       )
     );
