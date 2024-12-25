6e30963c7299b7d3057a5037775f9a7bd7ecb850
*vivek <vrvivekofficial@gmail.com>
2024-12-25 10:29:15 +0530

    add status code user login API

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -91,7 +91,7 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return next(new ErrorHandler("Please fill full form."));
+    return next(new ErrorHandler("Please fill full form.", 400));
   }
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
