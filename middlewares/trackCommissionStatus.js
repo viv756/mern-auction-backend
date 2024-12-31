153abfb2abe0e71066a998f80fe627bfa097ffab
*vivek <vrvivekofficial@gmail.com>
2024-12-31 12:59:16 +0530

    fixed the bug in the trackCommission status middleware

--- middlewares/trackCommissionStatus.js
+++ middlewares/trackCommissionStatus.js
@@ -1,12 +1,12 @@
 import User from "../models/user.model.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
-import { errorMiddleware } from "./errorHandler.js";
+import ErrorHandler from "./errorHandler.js";
 
 export const trackCommissionStatus = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user._id);
   if (user.unpaidCommission > 0) {
     return next(
-      new errorMiddleware(
+      new ErrorHandler(
         "You have unpaid commissions. Please pay them before posting a new auction",
         403
       )
